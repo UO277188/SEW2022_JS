81a2b3632c6967f11a7c29ff485240b63a23d1ec
uo277188 <uo277188@uniovi.es>
2022-11-07 14:12:33 +0100

    Ej1 + Ej2

---
+++ PRACTICA-3/Ejercicio-01/Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2022-2023";
+asignatura.alumno = "Diego Villa García";
+asignatura.email = "uo277188@uniovi.es";
---
+++ PRACTICA-3/Ejercicio-02/MasInfoNavegador.js
@@ -0,0 +1,8 @@
+document.write("Versión: ")
+document.write(infoNavegador.version);
+document.write(", plataforma: ")
+document.write(infoNavegador.plataforma);
+document.write(", vendedor: ")
+document.write(infoNavegador.vendedor);
+document.write(", agente: ")
+document.write(infoNavegador.agente);
