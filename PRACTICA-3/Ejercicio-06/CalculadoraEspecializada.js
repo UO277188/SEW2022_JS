ae1100f87881fb0aa07ad4b5b9ad0a36fad9e4e5
uo277188 <uo277188@uniovi.es>
2022-11-21 14:30:08 +0100

    costes energia

--- PRACTICA-3/Ejercicio-06/CalculadoraEspecializada.js
+++ PRACTICA-3/Ejercicio-06/CalculadoraEspecializada.js
@@ -1,7 +1,48 @@
 "use strict"
 class CalculadoraEspecializada extends CalculadoraRPN {
+    constructor(){
+        super();
+        document.addEventListener('keydown', (event) => { 
+            if(event.key=="E")
+                this.calcularEnergiaDispositivoConIVA();
+        });
+    }
+
+    energia(tipo){
+        this.pila.apilar(tipo);
+        var textArea = document.getElementsByTagName("textarea")[0];
+        textArea.innerHTML = this.pila.mostrar();
+    }
+
+    calcularEnergiaDispositivoConIVA(){
+        if (this.pila.tamaño() < 4)
+            return;
+        try {
+            var energia = this.pila.desapilar();
+            var iva = Number(this.pila.desapilar());
+            var horas = Number(this.pila.desapilar());
+            var dias = Number(this.pila.desapilar());
+
+            var precioEnergia = 0;
+            if(energia == "gas")
+                precioEnergia = Number(0.1503);
+            else if(energia == "solar")
+                precioEnergia = Number(0.343);
+            else if(energia == "eolica")
+                precioEnergia = Number(0.06);
+            else if(energia == "nuclear")
+                precioEnergia = Number(0.04);
+
+            var total = dias * horas * precioEnergia;
+
+            this.pila.apilar(Number(total + total * iva/100));
+            this.digito = "";
+            document.getElementsByTagName("textarea")[0].innerHTML = this.pila.mostrar();
+        } catch (error) {
+            this.borrar();
+        }
+    }
 
 }
-
 
 var calc = new CalculadoraEspecializada();
