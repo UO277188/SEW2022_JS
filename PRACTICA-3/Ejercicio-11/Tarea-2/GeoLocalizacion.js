e3276d11a3a227a8825109c6643df8ccbc1d1c4f
uo277188 <uo277188@uniovi.es>
2022-11-28 14:25:18 +0100

    ej 11 section + tarea 2

--- PRACTICA-3/Ejercicio-11/Tarea-2/GeoLocalizacion.js
+++ PRACTICA-3/Ejercicio-11/Tarea-2/GeoLocalizacion.js
@@ -1,10 +1,11 @@
 "use strict";
 class GeoLocalización {
     constructor (){
-        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.errores.bind(this));
     }
 
     getPosicion(posicion){
+        this.error = "Localización encontraada correctamente";
         this.longitud         = posicion.coords.longitude; 
         this.latitud          = posicion.coords.latitude;  
         this.precision        = posicion.coords.accuracy;
@@ -17,6 +18,12 @@ class GeoLocalización {
     mostrarPosicion(){
         var ubicacion = document.getElementsByTagName("section")[0];
         var datos='<h2>Datos</h2>'; 
+        if(this.error==""){
+            datos+=
+            ubicacion.innerHTML = datos;
+            return;
+        }
+        datos+='<p>'+this.error+'</p>';
         datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
         datos+='<p>Latitud: '+this.latitud +' grados</p>';
         datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
@@ -25,6 +32,23 @@ class GeoLocalización {
         datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
         datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
         ubicacion.innerHTML = datos;
+    }
+
+    errores(error){
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                this.error = "Permiso de geolocalización rechazado";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                this.error = "La información de geolocalización no está disponible";
+                break;
+            case error.TIMEOUT:
+                this.error = "Petición de geolocalización caducada";
+                break;
+            case error.UNKNOWN_ERROR:
+                this.error = "Se produjo un error al procesar la geolocalización";
+                break;
+            }
     }
 }
 
