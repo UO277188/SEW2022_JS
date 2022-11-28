e1c27ee2e9ab450a38cb43e27f57c04ad6ee10bb
uo277188 <uo277188@uniovi.es>
2022-11-28 14:02:23 +0100

    Ej 11 tarea 1

---
+++ PRACTICA-3/Ejercicio-11/Tarea-1/GeoLocalizacion.js
@@ -0,0 +1,31 @@
+"use strict";
+class GeoLocalización {
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+    }
+
+    getPosicion(posicion){
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+
+    mostrarPosicion(){
+        var ubicacion = document.getElementsByTagName("ul")[0];
+        var datos=''; 
+        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
+        datos+='<p>Latitud: '+this.latitud +' grados</p>';
+        datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
+        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
+        datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>'; 
+        datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
+        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
+        ubicacion.innerHTML = datos;
+    }
+}
+
+var geolocalizacion = new GeoLocalización();
